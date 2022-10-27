12a031e72f8c329639d91ceaab6597bcee85ffa1
chenxy <cxy20000517@gmail.com>
2022-10-27 15:51:12 -0400

    first commit

---
+++ P0/app/src/main/java/ec500/hw2/p0/MainActivity.java
@@ -0,0 +1,14 @@
+package ec500.hw2.p0;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
